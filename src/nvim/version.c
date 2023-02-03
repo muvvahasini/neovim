f27db8094e72721bda287d3beb492617f321d015
Hasini. Muvva <116424939+muvvahasini@users.noreply.github.com>
2023-02-03 10:50:28 +0530

    modify this text in neovim

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -2788,7 +2788,7 @@ void intro_message(int colon)
   static char *(lines[]) = {
     N_(NVIM_VERSION_LONG),
     "",
-    N_("Nvim is open source and freely distributable"),
+    N_("Nvim is [Test:hasini muvva] open source and freely distributable"),
     "https://neovim.io/#chat",
     "",
     N_("type  :help nvim<Enter>       if you are new! "),
